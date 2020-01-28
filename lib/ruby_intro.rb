61b0cbd12634464629c1539daf7e4a57dd6cf9ec
dtrej9999 <dtrej9999@tamu.edu>
2020-01-28 11:54:34 -0600

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,26 +2,65 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  sum = 0
+  arr.each{|num| sum += num}
+  sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  sum = 0
+  
+  if arr.length > 1
+    max1 = arr.at(0)
+    max2 = arr.at(1)
+    
+    arr2 = arr.drop(2)
+    
+    arr2.each{|val|
+      if (val > max1) && (max2 > max1)
+        max1 = val
+      elsif (val > max2) && (max1 > max2)
+        max2 = val
+      end
+    }
+    sum = max1 + max2
+  elsif arr.length == 1
+    sum = arr.at(0)
+  else
+    sum
+  end
+  
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  values = Hash.new
+  ans = false
+  
+  arr.each{|val|
+    nComp = n - val
+    
+    if(values.has_key?(nComp))
+      ans = true
+    end
+    
+    values[val] = 1
+  }
+  ans
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, "
+  str.concat(name)
+  str
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  ans = /\A[^aeiouAEIOU]/.match?(s)
+  puts ans
+  ans
 end
 
 def binary_multiple_of_4? s
